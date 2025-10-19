8938d9e495829a3dac5023a2287694ef3ffea58e
Legend1381 <mostafa.nazari9494@gmail.com>
2025-10-19 19:01:28 +0330

    make plugins and configs ready to start

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,21 @@
-import vuetify from 'eslint-config-vuetify'
+import baseConfig from "eslint-config-vuetify";
 
-export default vuetify()
+export default [
+  ...baseConfig,
+  {
+    rules: {
+      "@stylistic/quotes": ["error", "double"],
+      "vue/script-indent": [
+        "error",
+        2,
+        {
+          baseIndent: 0,
+          switchCase: 0,
+          ignores: [],
+        },
+      ],
+      "vue/attributes-order": "warn",
+      "vue/order-in-components": "warn",
+    },
+  },
+];
